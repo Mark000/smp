3e21466a5286dd2f3c8e1e39f7cedb4c9ee07afc
Marko Oksanen <me@markooksanen.com>
2016-05-29 23:42:55 +0200

    experimenting with static html

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require("express");
 var app     = express();
 var path    = require("path");
 
+app.use(express.static('template'));
 
 app.get('/',function(req,res){
 //  res.send("hello");
@@ -23,12 +24,12 @@ app.get('/product/examplepdp',function(req,res){
   res.sendFile('products/product.html', { root: __dirname });
 });
 
+/*
 app.get('/template',function(req,res){
 //  res.send("hello");
   res.sendFile('template/home-1-clothes-full-width.html', { root: __dirname });
 });
-
-
+*/
 
 app.listen(process.env.PORT || 5000);
 
